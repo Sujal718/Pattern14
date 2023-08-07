5ce90f6be8385ecfd8ec739784c0ef05547aac74
Sujal718 <104685635+Sujal718@users.noreply.github.com>
2023-08-08 00:52:37 +0530

    Add files via upload

---
+++ pattern14.cpp
@@ -0,0 +1,22 @@
+/*
+A B C
+B C D
+C D E*/
+
+#include<iostream>
+using namespace std;
+int main(){
+ int i=1,n;
+ cin>>n;
+
+ while(i<=n){
+    int j=1;
+    while(j<=n){
+         char ch='A'+i+j-2;
+        cout<<ch<<" ";
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+ }
+}
